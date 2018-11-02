694cea01e6a2cd893a3984851e678b8ac7970010
JakeMaldonado <jakemaldonado@protonmail.com>
2018-11-02 11:21:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# email-demo-chrome-extension
---
+++ content.js
@@ -0,0 +1,4 @@
+var emails = document.getElementsByClassName('.inboxsdk__thread_row');
+for (var i = 0, l = emails.length; i < l; i++) {
+  emails[i].style.color = 'white';
+}
